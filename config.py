5c910c905ffec224d7aca3552068c399261e421c
Dhiraj Dhungana <dhirajdhunganaofficial@gmail.com>
2026-05-26 18:47:37 +1000

    mysql username and password for server

--- config.py
+++ config.py
@@ -2,7 +2,8 @@
 class Config:
 
     SQLALCHEMY_DATABASE_URI = \
-    "mysql+pymysql://root:@localhost/travel_blog"
-    # "mysql+pymysql://bloguser:StrongPass123@localhost/travel_blog"
+    "mysql+pymysql://bloguser:StrongPass123@localhost/travel_blog"
+
+    # "mysql+pymysql://root:@localhost/travel_blog"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
